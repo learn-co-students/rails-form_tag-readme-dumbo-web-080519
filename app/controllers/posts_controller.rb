0ebeef4044235039e133a62f2f4aa48801accaac
Josh Nho <joshnho@gmail.com>
2019-09-03 15:10:02 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,19 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    render :index
+  end
+
+  def show
+    @posts = Post.find_by(params[:id])
+  end
+
+  def new
+  end
+  
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+  redirect_to posts_path
   end
   
 end
