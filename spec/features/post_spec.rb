1d9664403ab7c50507f56ae8f113b942ddfdfcd5
Josh Nho <joshnho@gmail.com>
2019-09-03 15:14:29 -0400

    ??

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 describe 'new post' do
-<<<<<<< HEAD
     it 'ensures that the form route works with the /new action' do
       visit new_post_path
       expect(page.status_code).to eq(200)
